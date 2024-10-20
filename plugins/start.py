19b3a68f8885befa045041c589cb20b86d8ca8d3
Sukuna69x <niloygain29@gmail.com>
2024-10-20 23:42:20 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,4 +1,4 @@
-import os, asyncio, humanize
+import os, asyncio
 from pyrogram import Client, filters, __version__
 from pyrogram.enums import ParseMode
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
